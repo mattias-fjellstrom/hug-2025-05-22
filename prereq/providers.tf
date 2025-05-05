e8d1f07ecd66e0e45878e20fb4faaf1742ca280e
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2025-05-05 19:15:13 +0200

    Remove unused prereqs

--- prereq/providers.tf
+++ prereq/providers.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.97.0"
-    }
-
     hcp = {
       source  = "hashicorp/hcp"
       version = "~> 0.104.0"
@@ -14,14 +9,5 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.5.2"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.7.2"
-    }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
